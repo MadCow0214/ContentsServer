1932e1eff853198057fcf66c1cadcd93e0a879bf
sonhansung <sonhansung@gmail.com>
2023-11-26 21:17:23 +0900

    update version

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,8 +1,8 @@
 import multer from "multer";
 import multerS3 from "multer-s3";
-import S3 from "aws-sdk/clients/s3";
+import { S3Client } from "@aws-sdk/client-s3";
 
-const s3 = new S3({
+const s3 = new S3Client({
   accessKeyId: process.env.AWS_KEY,
   secretAccessKey: process.env.AWS_SECRET,
   region: "ap-northeast-2"
@@ -12,14 +12,14 @@ const multerVideo = multer({
   storage: multerS3({
     s3,
     acl: "public-read",
-    bucket: "sonhansung-contents/video"
+    bucket: process.env.S3_BUCKET_NAME + "/video"
   })
 });
 const multerImage = multer({
   storage: multerS3({
     s3,
     acl: "public-read",
-    bucket: "sonhansung-contents/image"
+    bucket: process.env.S3_BUCKET_NAME + "/image"
   })
 });
 

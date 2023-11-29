b136868bf09349a58ab8653c062cf3880839357b
sonhansung <sonhansung@gmail.com>
2023-11-29 11:24:25 +0900

    Update middlewares.js

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,6 +1,7 @@
 import multer from "multer";
 import multerS3 from "multer-s3";
 import { S3Client } from "@aws-sdk/client-s3";
+import path from 'path';
 
 const s3 = new S3Client({
   accessKeyId: process.env.AWS_KEY,
@@ -12,14 +13,20 @@ const multerVideo = multer({
   storage: multerS3({
     s3,
     acl: "public-read",
-    bucket: process.env.S3_BUCKET_NAME + "/video"
+    bucket: process.env.S3_BUCKET_NAME,
+    key: function (req, file, cb) {
+      cb(null, `video/${Date.now()}_${path.basename(file.originalname)}`)
+    },
   })
 });
 const multerImage = multer({
   storage: multerS3({
     s3,
     acl: "public-read",
-    bucket: process.env.S3_BUCKET_NAME + "/image"
+    bucket: process.env.S3_BUCKET_NAME,
+    key: function (req, file, cb) {
+      cb(null, `image/${Date.now()}_${path.basename(file.originalname)}`)
+    },
   })
 });
 
